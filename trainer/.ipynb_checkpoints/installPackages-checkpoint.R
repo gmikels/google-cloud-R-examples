080a18a8fa8dce7d78fc4e75aa98a2057d06350f
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-04-06 23:18:42 +0000

    remove ipynb checkpoint files

--- trainer/.ipynb_checkpoints/installPackages-checkpoint.R
+++
@@ -1,8 +0,0 @@
-# Set library path and repo
-options(repos = "http://cran.us.r-project.org")
-.libPaths(Sys.getenv("LIBS"))
-
-# Install required libraries
-install.packages("glmnet", quiet=TRUE)
-install.packages("caret", quiet=TRUE)
-install.packages("argparse", quiet=TRUE)
